be6e634221a7327b27672bd9ad64e34288200bfa
Thomas Eriksson <thomas.eriksson@gmail.com>
2025-09-03 16:50:10 +0200

    feat: add default colors

--- monorail.sh
+++ monorail.sh
@@ -254,7 +254,7 @@ local RGB_CUR_COLOR RGB_CUR_R RGB_CUR_GB RGB_CUR_G RGB_CUR_B
 if [[ $_MONORAIL_CACHE != "$COLUMNS$_MONORAIL_TEXT" ]];then
 unset _MONORAIL_DATE _MONORAIL_CACHE "_PROMPT_LUT[*]" "_PROMPT_TEXT_LUT[*]" _MEASURE
 if [[ ! -f "$_MONORAIL_CONFIG/colors-$_MONORAIL_SHORT_HOSTNAME".sh ]];then
-LC_ALL=C LC_MESSAGES=C \cp "$_MONORAIL_DIR"/gradients/Default.sh "$_MONORAIL_CONFIG/colors-$_MONORAIL_SHORT_HOSTNAME".sh >&- 2>&-
+LC_ALL=C LC_MESSAGES=C \cat "$_MONORAIL_DIR"/colors/Default.sh "$_MONORAIL_DIR"/gradients/Default.sh > "$_MONORAIL_CONFIG/colors-$_MONORAIL_SHORT_HOSTNAME".sh 2>&-
 fi
 # shellcheck disable=SC1090,SC1091 # file will be copied
 . "$_MONORAIL_CONFIG/colors-$_MONORAIL_SHORT_HOSTNAME".sh
