692a6598bc98adc25543cd66abc339fb492209a5
Thomas Eriksson <thomas.eriksson@gmail.com>
2025-09-10 14:03:22 +0200

    bugfix: general improvements

--- monorail.lite.sh
+++ monorail.lite.sh
@@ -23,11 +23,48 @@
 # busybox sh
 # osh
 # brush
-
+CR=$(echo 0d | xxd -r -p)
+ESC=$(echo 1b | xxd -r -p)
+BEL=$(echo 07 | xxd -r -p)
 # serial terminals do not set SIZE
 [ -z "$COLUMNS" ] && COLUMNS=78
 case "$TERM" in
-"xterm"* | "alacritty")
+"xterm"* | "alacritty"*)
+	_MONORAIL_XTERM_TERMINAL=1
+	# UTF-8 "Lower one eighth block"
+	case "$LANG" in
+	*.UTF-8) _MONORAIL_LINE_SEGMENT=$(echo E29681 | xxd -p -r) ;;
+	*) _MONORAIL_LINE_SEGMENT=_ ;;
+	esac
+	;;
+"vt"???)
+	_MONORAIL_VTXXX_TERMINAL=1
+	;;
+"wyse60")
+	_MONORAIL_REVERSE="$_MONORAIL_PREHIDE$(echo 1b4734 | xxd -p -r)$_MONORAIL_POSTHIDE"
+	_MONORAIL_NORMAL="$_MONORAIL_PREHIDE$(echo 1b281b48031b47301b6344 | xxd -p -r)$_MONORAIL_POSTHIDE"
+	bind 'set enable-bracketed-paste off'
+	_MONORAIL_WYSE60_TERMINAL=1
+	_MONORAIL_DUMB_TERMINAL=1
+	;;
+"tek"* | "ibm-327"* | "dp33"?? | "dumb" | "dm2500" | "adm3a" | "vt"??)
+	bind 'set enable-bracketed-paste off'
+	_MONORAIL_DUMB_TERMINAL=1
+	_MONORAIL_NORMAL="!"
+	;;
+*)
+	_MONORAIL_NORMAL="|"
+	;;
+esac
+if [ "$_MONORAIL_XTERM_TERMINAL" ] || [ "$_MONORAIL_VTXXX_TERMINAL" ]; then
+	# workaround for shellcheck and zsh parsers
+	_MONORAIL_REVERSE="[7m$_MONORAIL_REVERSE"
+	_MONORAIL_REVERSE="$_MONORAIL_PREHIDE$ESC$_MONORAIL_REVERSE"
+	_MONORAIL_NORMAL="[0m$_MONORAIL_POSTHIDE"
+	_MONORAIL_NORMAL="$_MONORAIL_PREHIDE$ESC$_MONORAIL_NORMAL"
+fi
+case "$TERM" in
+"xterm"* | "alacritty"*)
 	# UTF-8 "Lower one eighth block"
 	case "$LANG" in
 	*.UTF-8) _MONORAIL_LINE_SEGMENT=$(echo E29681 | xxd -p -r) ;;
@@ -56,12 +93,10 @@ elif [ -n "$BASH_VERSION" ] || [ -n "$OKSH_VERSION" ]; then
 fi
 [ -f /usr/lib/git-core/git-sh-prompt ] && . /usr/lib/git-core/git-sh-prompt 2>/dev/null
 _MONORAIL_UPDATE() {
-#((6832f8c...))
-#(branchname)
 	_MONORAIL_GIT_PS1=$(
 		TERM=dumb GIT_CONFIG_GLOBAL="" LC_MESSAGES=C LC_ALL=C __git_ps1 ""
 	)
-    # some shells do not set PWD
+	# some shells do not set PWD
 	PWD=$(pwd)
 	PWD_BASENAME=$(basename "$PWD")
 	[ -z "$PWD_BASENAME" ] && PWD_BASENAME=/
@@ -74,8 +109,7 @@ _MONORAIL_UPDATE() {
 	I=0
 	LINE_WIDTH=$COLUMNS
 	_MONORAIL_LINE=
-	CR=$(echo 0d | xxd -r -p)
-	ESC=$(echo 1b | xxd -r -p)
+
 	if [ -n "$_MONORAIL_VTXXX_TERMINAL" ]; then
 		# double width line in VTXXX terminals for faster drawing
 		# line needs to be broken due to zsh having issue parsing it
@@ -112,19 +146,12 @@ _MONORAIL_UPDATE() {
 	else
 		LINE_WIDTH=$COLUMNS
 	fi
-	if [ "$_MONORAIL_XTERM_TERMINAL" ] || [ "$_MONORAIL_VTXXX_TERMINAL" ] || [ "$TERM" = "wyse60" ]; then
-		_MONORAIL_REVERSE="$_MONORAIL_PREHIDE$(LC_MESSAGES=C LC_ALL=C tput rev 2>/dev/null)$_MONORAIL_POSTHIDE"
-		_MONORAIL_NORMAL="$_MONORAIL_PREHIDE$(LC_MESSAGES=C LC_ALL=C tput sgr0 2>/dev/null)$_MONORAIL_POSTHIDE"
-	elif [ "$_MONORAIL_DUMB_TERMINAL" ]; then
-		_MONORAIL_NORMAL="!"
-	else
-		_MONORAIL_NORMAL="|"
-	fi
+
 	PS1="$_MONORAIL_LINE
 $_MONORAIL_REVERSE$_MONORAIL_TEXT$_MONORAIL_NORMAL "
-    # update the prompt when the user calls 'cd'
+	# update the prompt when the user calls 'cd'
 	cd() {
-        # need to set/unset 'cd()' since not all shell have `builtin`
+		# need to set/unset 'cd()' since not all shell have `builtin`
 		unset -f cd 2>/dev/null
 		if [ "$1" ]; then
 			cd "$1" || return $?
@@ -137,6 +164,31 @@ $_MONORAIL_REVERSE$_MONORAIL_TEXT$_MONORAIL_NORMAL "
 # update monorail on window resizing
 trap "_MONORAIL_UPDATE" WINCH
 
+# shellcheck disable=SC2329
+__git_ps1() { :; }
+
+_ICON() {
+	ICON="$1"
+	shift
+	case "$LANG" in *.UTF-8)
+		# shellcheck disable=SC2086 # shellcheck incorrectly misses qoutes
+		_TITLE "$ICON  $(basename \"$1\")"
+		;;
+	*)
+		# shellcheck disable=SC2086 # shellcheck incorrectly misses qoutes
+		_TITLE "$(basename \"$1\")"
+		;;
+	esac
+	"$@"
+}
+if [ -n "$_MONORAIL_XTERM_TERMINAL" ]; then
+	_TITLE() {
+		# /dev/tty bypasses stdout/stdin so redirection is not disturbed
+		printf "$ESC]0;%s$BEL" "$*" >/dev/tty 2>/dev/null
+	}
+else
+	_TITLE() { :; }
+fi
 if [ "$ZSH_NAME" ]; then
 	precmd() {
 		_MONORAIL_UPDATE
@@ -148,51 +200,6 @@ elif [ -n "$BASH_VERSION" ]; then
 	else
 		PROMPT_COMMAND="_MONORAIL_UPDATE"
 	fi
-fi
-# shellcheck disable=SC2329
-__git_ps1() { :; }
-case "$TERM" in
-"xterm"* | "alacritty"*)
-	_MONORAIL_XTERM_TERMINAL=1
-	;;
-"vt"???)
-	_MONORAIL_VTXXX_TERMINAL=1
-	;;
-"tek"* | "ibm-327"* | "dp33"?? | "dumb" | "wyse60" | "dm2500" | "adm3a" | "vt"??)
-	bind 'set enable-bracketed-paste off'
-	_MONORAIL_DUMB_TERMINAL=1
-	;;
-esac
-_ICON(){
-local ICON="$1"
-shift
-if [[ -z ${FUNCNAME[1]} ]]||[[ ${FUNCNAME[1]} == "_NO_MEASURE" ]];then
-local FIRST_ARG="$1"
-(case "$FIRST_ARG" in
-_*)shift
-esac
-FIRST_ARG="$1"
-FIRST_NON_OPTION="$2"
-while [[ ${FIRST_NON_OPTION:0:1} == '-' ]]||[ "${FIRST_NON_OPTION:0:1}" = '_' ]||[ "$FIRST_NON_OPTION" = '.' ];do
-if [ "$FIRST_NON_OPTION" = '-u' ];then
-shift 2
 else
-shift
+	_MONORAIL_UPDATE
 fi
-FIRST_NON_OPTION="$2"
-done
-if [ -z "$FIRST_NON_OPTION" ];then
-_TITLE "$ICON  ${FIRST_ARG##*/}"
-else
-_TITLE "$ICON  ${FIRST_NON_OPTION##*/}"
-fi) >&- 2>&-
-fi
-"$@"
-}
-case "$TERM" in
-"xterm"*|"alacritty")
-_TITLE(){
-printf "\e]0;%s\a" "$*" >/dev/tty 2>/dev/null 
-}
-esac
-_MONORAIL_UPDATE
