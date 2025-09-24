30d48de20d324bba059cea6a1a3d2d7ebb3c2ccc
Thomas Eriksson <thomas.eriksson@gmail.com>
2025-09-24 17:05:48 +0200

    feat: add horizontal bar for better readability when copying to a document or chat

--- monorail.sh
+++ monorail.sh
@@ -342,10 +342,13 @@ fi
 "${_COLORS[13]}" \
 "${_COLORS[14]}" \
 "${_COLORS[15]}"
+
+# the invisible vertical bar is added to make the prompt displayed better when copied to a chat or text doc
+# this is not normally visible, but on some terminals not supporting ^[8m it will fall back to black
 # shellcheck disable=SC2025,SC1078,SC1079 # no need to enclose in \[ \] as cursor position is calculated from after newline, quoting is supposed to span multiple lines
 # shellcheck disable=SC2025,SC1078,SC1079
 PS1=$'\e'"]0;"'$TITLE'$'\a'$'\r'$'\e'"[0m$_MONORAIL_LINE
-$_MONORAIL_PREHIDE$_MONORAIL_ATTRIBUTE$_MONORAIL_POSTHIDE$_MONORAIL_TEXT_FORMATTED$_MONORAIL_PREHIDE"$'\e'"[0m"$'\e'"[?25h$_MONORAIL_POSTHIDE "
+$_MONORAIL_PREHIDE$_MONORAIL_ATTRIBUTE$_MONORAIL_POSTHIDE$_MONORAIL_TEXT_FORMATTED$_MONORAIL_PREHIDE"$'\e'"[0m"$'\e'"[30m"$'\e'"[8m${_MONORAIL_POSTHIDE}▎$_MONORAIL_PREHIDE"$'\e'"[0m"$'\e'"[?25h${_MONORAIL_POSTHIDE}"
 unset _MONORAIL_NOSTYLING
 }
 _TITLE(){
