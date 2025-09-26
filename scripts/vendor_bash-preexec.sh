72746b3fab86e65923bbcfd12ed85ebf55f71bb8
Thomas Eriksson <thomas.eriksson@gmail.com>
2025-09-26 16:45:00 +0200

    feat: reformat vendor_bash-preexec

--- scripts/vendor_bash-preexec.sh
+++ scripts/vendor_bash-preexec.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 rm -rf bash-preexec
 git clone https://github.com/rcaloras/bash-preexec
-REVISION=$(cd bash-preexec; git rev-parse HEAD;)
+REVISION=$(
+	cd bash-preexec
+	git rev-parse HEAD
+)
 rm -rf bash-preexec/.git
 #git add bash-preexec
 #git commit -m "feat: vendor bash-preexec at $REVISION"
