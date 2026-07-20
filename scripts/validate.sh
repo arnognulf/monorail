d28931c1391a45f8aeb0b27c3351c6ba4778257e
Thomas Eriksson <thomas.eriksson@gmail.com>
2026-07-20 11:18:04 +0200

    wip: preprocessor

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 . scripts/sandbox.inc.sh
+set -x
+rm -f monorail.bash
+cat monorail.common.in.sh |
+	sed 's/@PROMPT_PREHIDE@/\\[/g' |
+	sed 's/@PROMPT_POSTHIDE@/\\]/g' \
+		>monorail.bash
 
-# do not format monorail.sh
+rm -f monorail.zsh
+cat monorail.common.in.sh |
+	sed 's/@PROMPT_PREHIDE@/%{/g' |
+	sed 's/@PROMPT_POSTHIDE@/%}/g' \
+		>monorail.zsh
+
+# do not format monorail.bash
 _SANDBOX shellcheck -x monorail.bash
 _SANDBOX shellcheck -x monorail.sh
 _SANDBOX_RWCWD shfmt -w monorail.sh
